601c4071804b2929ed663a7416f60d0de7a97b2f
Tiago Valadares <tiago.valadares@wabtec.com>
2022-11-14 19:39:05 -0300

    Changes on HMC5883L.h

--- HMC5883L.c
+++ HMC5883L.c
@@ -22,5 +22,160 @@
 * Departamento de Engenharia Eletrônica”.
 */
 
+/*
+NOTES:
+*** Polling mode IO operation ***
+    =================================
+    [..]
+      (+) Transmit in master mode an amount of data in blocking mode using @ref HAL_I2C_Master_Transmit()
+      (+) Receive in master mode an amount of data in blocking mode using @ref HAL_I2C_Master_Receive()
+      (+) Transmit in slave mode an amount of data in blocking mode using @ref HAL_I2C_Slave_Transmit()
+      (+) Receive in slave mode an amount of data in blocking mode using @ref HAL_I2C_Slave_Receive()
+
+*/
+
+/**
+  * brief  Transmits in master mode an amount of data in blocking mode.
+  * param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
+  *                the configuration information for the specified I2C.
+  * param  DevAddress Target device address: The device 7 bits address value
+  *         in datasheet must be shifted to the left before calling the interface
+  * param  pData Pointer to data buffer
+  * param  Size Amount of data to be sent
+  * param  Timeout Timeout duration
+  * retval HAL status
+  
+HAL_StatusTypeDef HAL_I2C_Master_Transmit(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+*/
+
+/**
+  * brief  Receives in master mode an amount of data in blocking mode.
+  * param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
+  *                the configuration information for the specified I2C.
+  * param  DevAddress Target device address: The device 7 bits address value
+  *         in datasheet must be shifted to the left before calling the interface
+  * param  pData Pointer to data buffer
+  * param  Size Amount of data to be sent
+  * param  Timeout Timeout duration
+  * retval HAL status
+  
+HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+*/
+
+#include "HMC5883L.h"
+
+extern I2C_HandleTypeDef hi2c1;
+
+#define SLAVE_ADDRESS_LCD HMC5883L_ADDRESS
+
+#define TIMEOUT 100
+
+HAL_I2C_Master_Transmit (&hi2c1, HMC5883L_ADDRESS,(uint8_t *) data_t, 4, 100);
+HAL_I2C_Master_Receive (&hi2c1, HMC5883L_ADDRESS,(uint8_t *) data_t, 4, 100);
+
+HAL_Delay(50);  // wait for >40ms
+
+void write_register(uint8_t register_pointer, uint8_t register_value)
+{
+    uint8_t data[2];
+
+    data[0] = register_pointer;
+    data[1] = register_value;
+
+    HAL_I2C_Master_Transmit(&hi2c1, HMC5883L_ADDRESS, data, 2, 100);  // data is the start pointer of our array
+}
 
 
+void read_register(uint8_t register_pointer, uint8_t* receive_buffer)
+{
+    // first set the register pointer to the register wanted to be read
+    HAL_I2C_Master_Transmit(&hi2c1, HMC5883L_ADDRESS, &register_pointer, 1, 100); 
+
+    // receive the 1 x 8bit data into the receive buffer
+    HAL_I2C_Master_Receive(&hi2c1, HMC5883L_ADDRESS, receive_buffer, 1, 100);   
+}
+
+
+// Configuration Functions
+
+// Configuration Register A
+
+uint8_t HMC5883L_getSampleAveraging(){
+  // TODO
+  I2Cdev_readBits(devAddr, HMC5883L_RA_CONFIG_A, HMC5883L_CRA_AVERAGE_BIT, HMC5883L_CRA_AVERAGE_LENGTH, buffer);
+  return buffer[0];
+}
+
+void HMC5883L_setSampleAveraging(uint8_t averaging){
+  //TODO
+  uint8_t data_t[4];
+  write_register(HMC5883L_RA_CONFIG_A, ); 
+  HAL_I2C_Master_Transmit (&hi2c1, SLAVE_ADDRESS_LCD,(uint8_t *) data_t, 4, TIMEOUT);
+  I2Cdev_writeBits(devAddr, HMC5883L_RA_CONFIG_A, HMC5883L_CRA_AVERAGE_BIT, HMC5883L_CRA_AVERAGE_LENGTH, averaging);
+}
+
+uint8_t HMC5883L_getDataRate(){
+  //TODO
+  I2Cdev_readBits(devAddr, HMC5883L_RA_CONFIG_A, HMC5883L_CRA_RATE_BIT, HMC5883L_CRA_RATE_LENGTH, buffer);
+  return buffer[0];
+}
+
+void HMC5883L_setDataRate(uint8_t rate){
+  I2Cdev_writeBits(devAddr, HMC5883L_RA_CONFIG_A, HMC5883L_CRA_RATE_BIT, HMC5883L_CRA_RATE_LENGTH, rate);
+}
+
+uint8_t HMC5883L_getMeasurementMode(){
+  I2Cdev_readBits(devAddr, HMC5883L_RA_CONFIG_A, HMC5883L_CRA_BIAS_BIT, HMC5883L_CRA_BIAS_LENGTH, buffer);
+  return buffer[0];
+}
+
+void HMC5883L_setMeasurementMode(uint8_t bias){
+  I2Cdev_writeBits(devAddr, HMC5883L_RA_CONFIG_A, HMC5883L_CRA_BIAS_BIT, HMC5883L_CRA_BIAS_LENGTH, bias);
+}
+
+
+// CONFIG_B register
+uint8_t HMC5883L_getGain(){
+  I2Cdev_readBits(devAddr, HMC5883L_RA_CONFIG_B, HMC5883L_CRB_GAIN_BIT, HMC5883L_CRB_GAIN_LENGTH, buffer);
+  return buffer[0];
+}
+
+void HMC5883L_setGain(uint8_t gain){
+  I2Cdev_writeByte(devAddr, HMC5883L_RA_CONFIG_B, gain << (HMC5883L_CRB_GAIN_BIT - HMC5883L_CRB_GAIN_LENGTH + 1));
+}
+
+// MODE register
+uint8_t HMC5883L_getMode(){
+  I2Cdev_readBits(devAddr, HMC5883L_RA_MODE, HMC5883L_MODEREG_BIT, HMC5883L_MODEREG_LENGTH, buffer);
+  return buffer[0];
+}
+
+void HMC5883L_setMode(uint8_t mode){
+  I2Cdev_writeByte(devAddr, HMC5883L_RA_MODE, newMode << (HMC5883L_MODEREG_BIT - HMC5883L_MODEREG_LENGTH + 1));
+}
+
+// General Functions
+void HMC5883L_initialize(){
+  devAddr = HMC5883L_DEFAULT_ADDRESS;
+    // write CONFIG_A register
+    I2Cdev_writeByte(devAddr, HMC5883L_RA_CONFIG_A,
+        (HMC5883L_AVERAGING_8 << (HMC5883L_CRA_AVERAGE_BIT - HMC5883L_CRA_AVERAGE_LENGTH + 1)) |
+        (HMC5883L_RATE_15     << (HMC5883L_CRA_RATE_BIT - HMC5883L_CRA_RATE_LENGTH + 1)) |
+        (HMC5883L_BIAS_NORMAL << (HMC5883L_CRA_BIAS_BIT - HMC5883L_CRA_BIAS_LENGTH + 1)));
+
+    // write CONFIG_B register
+    HMC5883L_setGain(HMC5883L_GAIN_1090);
+
+    // write MODE register
+    HMC5883L_setMode(HMC5883L_MODE_SINGLE);
+}
+
+void HMC5883L_measurement(int16_t *x, int16_t *y, int16_t *z){
+  I2Cdev_readBytes(devAddr, HMC5883L_RA_DATAX_H, 6, buffer);
+  if (mode == HMC5883L_MODE_SINGLE){
+    I2Cdev_writeByte(devAddr, HMC5883L_RA_MODE, HMC5883L_MODE_SINGLE << (HMC5883L_MODEREG_BIT - HMC5883L_MODEREG_LENGTH + 1));
+  }
+  *x = (((int16_t)buffer[0]) << 8) | buffer[1];
+  *y = (((int16_t)buffer[4]) << 8) | buffer[5];
+  *z = (((int16_t)buffer[2]) << 8) | buffer[3];
+}
--- HMC5883L.h
+++ HMC5883L.h
@@ -24,33 +24,109 @@
 
 #include "stm32f1xx_hal.h"
 
-#define HMC5883L_ADDRESS      0x1E
+#define HMC5883L_ADDRESS          0x1E
 
-#define REG_CONFIG_A          0x00
-#define REG_CONFIG_B          0x01
-#define REG_MODE              0x02
-#define REG_DATA_OUT_X_MSB    0x03
-#define REG_DATA_OUT_X_LSB    0x04
-#define REG_DATA_OUT_Z_MSB    0x05
-#define REG_DATA_OUT_Z_LSB    0x06
-#define REG_DATA_OUT_Y_MSB    0x07
-#define REG_DATA_OUT_Y_LSB    0x08
-#define REG_STATUS            0x09
-#define REG_IDENT_A           0x0A
-#define REG_IDENT_B           0x0B
-#define REG_IDENT_C           0x0C
+#define REG_CONFIG_A              0x00
+#define REG_CONFIG_B              0x01
+#define REG_MODE                  0x02
+#define REG_DATA_OUT_X_MSB        0x03
+#define REG_DATA_OUT_X_LSB        0x04
+#define REG_DATA_OUT_Z_MSB        0x05
+#define REG_DATA_OUT_Z_LSB        0x06
+#define REG_DATA_OUT_Y_MSB        0x07
+#define REG_DATA_OUT_Y_LSB        0x08
+#define REG_STATUS                0x09
+#define REG_IDENT_A               0x0A
+#define REG_IDENT_B               0x0B
+#define REG_IDENT_C               0x0C
+
+// Configuration Register A
+// Layout for the bits of Configuration Register A (CRA)
+#define HMC5883L_CRA_SA_POS       6 // SAMPLE AVERAGE position on CRA
+#define HMC5883L_CRA_SA_TAM       2 // LENGTH of bits 
+#define HMC5883L_CRA_DATARATE_POS 4 // DATA RATE position on CRA
+#define HMC5883L_CRA_DATARATE_TAM 3
+#define HMC5883L_CRA_MM_POS       1 // Measurement Mode position on CRA
+#define HMC5883L_CRA_MM_TAM       2
+
+// Samples averaged
+#define HMC5883L_SAMPLE_AVERAGE_1 0x00 //(Default)
+#define HMC5883L_SAMPLE_AVERAGE_2 0x01
+#define HMC5883L_SAMPLE_AVERAGE_4 0x02
+#define HMC5883L_SAMPLE_AVERAGE_8 0x03
+
+// Typical Data Output Rate (Hz)
+#define HMC5883L_DATARATE_0_75_HZ 0x00
+#define HMC5883L_DATARATE_1_5HZ   0x01
+#define HMC5883L_DATARATE_3HZ     0x02
+#define HMC5883L_DATARATE_7_5HZ   0x03
+#define HMC5883L_DATARATE_15HZ    0x04 //(Default)
+#define HMC5883L_DATARATE_30HZ    0x05
+#define HMC5883L_DATARATE_75HZ    0x06
+
+// Measurement Mode
+#define HMC5883L_MEAS_MODE_NORMAL 0x00 //(Default)
+#define HMC5883L_MEAS_MODE_POS    0x01
+#define HMC5883L_MEAS_MODE_NEG    0x02
+
 
 // Configuration Register B
-#define HMC5883L_RANGE_0_88GA	0x00,
-#define HMC5883L_RANGE_1_3GA	0x20,
-#define HMC5883L_RANGE_1_9GA	0x40,
-#define HMC5883L_RANGE_2_5GA  0x60,
-#define HMC5883L_RANGE_4GA    0x80,
-#define HMC5883L_RANGE_4_7GA  0xA0,
-#define HMC5883L_RANGE_5_7GA  0xC0,
-#define HMC5883L_RANGE_8_1GA  0xE0
+// Layout for the bits of Configuration Register B (CRB)
+#define HMC5883L_CRB_RANGE_POS    7 // RANGE position on CRB
+#define HMC5883L_CRB_RANGE_TAM    3 // LENGTH of bits 
 
+// RANGE -> GAIN
+#define HMC5883L_RANGE_0_88GA   	0x00
+#define HMC5883L_RANGE_1_3GA	    0x01 //(Default)
+#define HMC5883L_RANGE_1_9GA	    0x02
+#define HMC5883L_RANGE_2_5GA      0x03
+#define HMC5883L_RANGE_4GA        0x04
+#define HMC5883L_RANGE_4_7GA      0x05
+#define HMC5883L_RANGE_5_7GA      0x06
+#define HMC5883L_RANGE_8_1GA      0x07
+
+// Mode Register
+// Layout for the bits of Mode Register
+#define HMC5883L_MODEREG_POS      1
+#define HMC5883L_MODEREG_TAM      2
+
+#define HMC5883L_MODE_CONTINUOUS    0x00
+#define HMC5883L_MODE_SINGLE        0x01
+#define HMC5883L_MODE_IDLE          0x02
+
+
+// Configuration Functions
+
+// Configuration Register A
+uint8_t HMC5883L_getSampleAveraging();
+void HMC5883L_setSampleAveraging(uint8_t averaging);
+uint8_t HMC5883L_getDataRate();
+void HMC5883L_setDataRate(uint8_t rate);
+uint8_t HMC5883L_getMeasurementMode();
+void HMC5883L_setMeasurementMode(uint8_t bias);
+
+// CONFIG_B register
+uint8_t HMC5883L_getGain();
+void HMC5883L_setGain(uint8_t gain);
+
+// MODE register
+uint8_t HMC5883L_getMode();
+void HMC5883L_setMode(uint8_t mode);
+
+// General Functions
 void HMC5883L_initialize();
 
-void HMC5883L_calibration();
+void HMC5883L_calibration(int16_t *x, int16_t *y, int16_t *z);
+
+void HMC5883L_measurement();
+
+void HMC5883L_north_inclination();
+
+void HMC5883L_setGain();
+
+void HMC5883L_setMode();
+
+
+
+
 
