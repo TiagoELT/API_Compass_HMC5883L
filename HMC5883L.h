51b9874e0cd32f576fed1c10a6ede1566877df99
Tiago Valadares <tiago.valadares@wabtec.com>
2022-11-14 16:17:51 -0300

    Initial Commit

---
+++ HMC5883L.h
@@ -0,0 +1,24 @@
+/*
+* Data: 14/11/2022
+* Universidade: UFMG - Universidade Federal de Minas Gerais
+* Autores: Eduarde e Tiago Rezende Valadares
+* Versão: 1.0
+* Tipo de licença:
+* Nome da api: HMC5883L
+* Arquivos: 
+*        - HMC5883L.c
+*        - HMC5883L.h
+* Requisitos de hardware: 
+*   - Módulo Bússola Eletrônica HMC5883L
+      Datasheet: 
+      https://www.generationrobots.com/media/module%20boussole%203%20axes%20HMC5883L/29133-HMC5883L-Compass-Module-IC-Documentation-v1.0.pdf
+*   - Kit NUCLEO 64 – STM32F103RB
+* Requisitos de software: 
+*   - STM32CubeIDE 1.6.1 
+*     Disponível em: https://www.st.com/en/development-tools/stm32cubeide.html
+*
+* Esta API foi desenvolvida como trabalho da disciplina de Programação de 
+* Sistemas Embarcados da UFMG – Prof. Ricardo de Oliveira Duarte – 
+* Departamento de Engenharia Eletrônica”.
+*/
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # API_Compass_HMC5883L
 API for the module HMC5883L used with the kit NUCLEO 64 – STM32F103RB
+
+API para uso com um Módulo Bússola Eletrônica HMC5883L.
+
+
+![alt text](https://github.com/TiagoELT/API_Compass_HMC5883L/blob/main/assets/image.jpg?raw=true)
+
